3e5abac53b3a8ac607cc1fe2e278d8f249a78d10
Stefan Schaar <stefan.schaar@tuta.io>
2024-05-21 16:07:59 +0200

    init repo

---
+++ src/main/java/com/example/jpahuser/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.example.jpahuser;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    public void destroy() {
+    }
+}
