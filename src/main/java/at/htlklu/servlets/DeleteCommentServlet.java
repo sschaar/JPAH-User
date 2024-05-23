883c868173449178341a6ddb265a059a173107f3
Stefan Schaar <stefan.schaar@tuta.io>
2024-05-23 10:51:24 +0200

    added try catch to DeleteCommentServlet

--- src/main/java/at/htlklu/servlets/DeleteCommentServlet.java
+++ src/main/java/at/htlklu/servlets/DeleteCommentServlet.java
@@ -6,17 +6,23 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name="deleteComment", value="/deleteComment")
 public class DeleteCommentServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
+        try {
+            Integer userId = Integer.valueOf(request.getParameter("userId"));
 
-        Integer userId = Integer.valueOf(request.getParameter("userId"));
+            Dao.deleteCommentsByUserId(userId);
 
-        Dao.deleteCommentsByUserId(userId);
+            response.sendRedirect(request.getContextPath() + "/index.jsp");
+        }catch(NumberFormatException ne){
+            PrintWriter out = response.getWriter();
+            out.println("Falsche Eingabe");
+        }
 
-        response.sendRedirect(request.getContextPath()+"/index.jsp");
-    }
+        }
 }
